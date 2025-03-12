335e97529f040be45a78022c1f575cea8ae2f59a
nhandeptraiii <thanhnhan21082003@gmail.com>
2025-03-12 20:27:17 +0700

    update xu ly loi

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const theoDoiMuonSachRoutes = require('./routes/theoDoiMuonSachRoutes');
 const nhaXuatBanRoutes = require('./routes/nhaXuatBanRoutes');
 const docGiaRoutes = require('./routes/docGiaRoutes');
 const nhanVienRoutes = require('./routes/nhanVienRoutes');
+const ApiError = require('./api-error');
 
 const app = express();
 
@@ -27,6 +28,21 @@ app.use('/api/muon-sach', theoDoiMuonSachRoutes);
 app.use('/api/nha-xuat-ban', nhaXuatBanRoutes);
 app.use('/api/doc-gia', docGiaRoutes);
 app.use('/api/nhan-vien', nhanVienRoutes);
+
+//handle 404 reponse
+app.use((req, res, next) => {
+    //Code ở đây sẽ chạy khi không có route được định nghĩa 
+    //khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi 
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    //Middleware xử lý lỗi tập trung 
+    //Trong các đoạn xử lý code ở route, gọi next(error) sẽ chuyển về middleware xử lý lỗi này
+    return res.status(error.statusCode || 500).json({
+        message : error.message || "Internal Server Error",
+    });
+});
 
 
 module.exports = app;
